4449ff6e6a76eeb4d60603bb40bd674110b81ff0
Nodzero <nackarin.boo@bru.ac.th>
2021-02-11 15:02:06 +0700

    edit setting rm 127-128

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
