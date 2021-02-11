39127a5560af78aa6e0bd205fe10e7cac58dc24d
Nodzero <nackarin.boo@bru.ac.th>
2021-02-11 14:58:55 +0700

    edit setting add 127-128

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static'
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
